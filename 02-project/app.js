84266c5c5f93fec05ada18b886c80923dff49a11
kemenykin <kemenykin@gmail.com>
2023-07-16 23:07:12 +0200

    objects, arrays, enums

--- 02-project/app.js
+++ 02-project/app.js
@@ -1,24 +1,7 @@
-// const person: {
-//     name: string;
-//     age: number;
-// } = {
-//     const person = {
-//     name: 'Kinga',
-//     age: 26,
-//     hobbies: ['sports', 'cooking'],
-//     role: [2, 'author']
-// }
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {}));
-;
-var person = {
-    name: 'Kinga',
-    age: 26,
-    hobbies: ['sports', 'cooking'],
-    role: Role.ADMIN
-};
-console.log(person.role);
+function add(n1, n2) {
+    return n1 + n2;
+}
+function printResult(num) {
+    console.log('Result' + num);
+}
+console.log(printResult(add(5, 10)));
