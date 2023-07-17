6c6c004b573d99da39bcfef7b1c174e37f4af0b0
kemenykin <kemenykin@gmail.com>
2023-07-17 10:20:05 +0200

    functions and types p1

--- 02-project/app.js
+++ 02-project/app.js
@@ -4,4 +4,13 @@ function add(n1, n2) {
 function printResult(num) {
     console.log('Result' + num);
 }
-console.log(printResult(add(5, 10)));
+var combineValues;
+combineValues = add;
+console.log(combineValues(8, 8));
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+addAndHandle(20, 10, function (result) {
+    console.log(result);
+});
--- 02-project/app.ts
+++ 02-project/app.ts
@@ -6,4 +6,16 @@ function printResult(num: number) {
     console.log('Result' + num);
 }
 
-console.log(printResult(add(5, 10)));
+let combineValues: (a: number, b: number) => number;
+combineValues = add;
+
+console.log(combineValues(8, 8));
+
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+    const result = n1 + n2;
+    cb(result);
+}
+
+addAndHandle(20, 10, (result) => {
+    console.log(result);
+})
