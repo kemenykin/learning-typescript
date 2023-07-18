9ec40bdce8da56aec23733f839740ec4641f3d60
kemenykin <kemenykin@gmail.com>
2023-07-18 11:11:39 +0200

    delete duplicated files

--- 02-project/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-let userInput;
-userInput = 'Kinga';
-userInput = 5;
-function generateError(message, code) {
-    throw { message: message, code: code };
-}
-generateError('An error occured', 500);
--- 02-project/app.ts
+++
@@ -1,9 +0,0 @@
-let userInput: unknown;
-userInput = 'Kinga';
-userInput = 5;
-
-function generateError(message: string, code: number) {
-    throw {message: message, code: code};
-}
-
-generateError('An error occured', 500);
