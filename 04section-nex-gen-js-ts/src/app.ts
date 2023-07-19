0ee4143fa091bbe4f53b8779af09288e4275603c
kemenykin <kemenykin@gmail.com>
2023-07-19 21:25:44 +0200

    added array destructuring

--- 04section-nex-gen-js-ts/src/app.ts
+++ 04section-nex-gen-js-ts/src/app.ts
@@ -18,3 +18,12 @@ const add = (...numbers: number[]) => {
 };
 
 const addedNumbers = add(5, 6, 7, 8, 10);
+
+// array without destructuring
+
+// const hobby1 = hobbies[0];
+// const hobby2 = hobbies[1];
+
+// destructured
+
+const [hobby1, hobby2] = hobbies;
