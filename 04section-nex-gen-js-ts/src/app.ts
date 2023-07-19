8871220f31bc25edf8dbfa602cdec84f8e9fe037
kemenykin <kemenykin@gmail.com>
2023-07-19 19:31:00 +0200

    reduce function added

--- 04section-nex-gen-js-ts/src/app.ts
+++ 04section-nex-gen-js-ts/src/app.ts
@@ -1,1 +1,14 @@
-// Code goes here!
+const person = {
+    name: 'Kinga',
+    age: 26
+}
+
+const copiedPerson = { ...person };
+
+const add = (...numbers: number[]) => {
+    return numbers.reduce((currentResult, currentValue) => { 
+        return currentResult + currentValue
+    }, 0)
+};
+
+const addedNumbers = add(5, 6, 7, 8, 10);
