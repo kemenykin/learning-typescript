13faa37de852b7ec809bb4fb46f4f46933561071
kemenykin <kemenykin@gmail.com>
2023-07-23 12:45:07 +0200

    interface practise

--- 05-classes-interfaces/src/app.js
+++ 05-classes-interfaces/src/app.js
@@ -1,36 +1,8 @@
-"use strict";
-class Department {
-    constructor(name, id) {
-        this.name = name;
-        this.id = id;
-        this.employees = [];
-    }
-    describe() {
-        console.log('Department: ' + this.name + this.id);
-    }
-    addEmployee(employee) {
-        this.employees.push(employee);
-    }
-}
-class ITDepartment extends Department {
-    constructor(id, admins) {
-        super(id, 'IT');
-        this.admins = admins;
-    }
-}
-const it = new ITDepartment('d1', ['Max']);
-class AccountingDepartment extends Department {
-    constructor(id, reports) {
-        super(id, 'Accounting');
-        this.reports = reports;
-    }
-    addReports(text) {
-        this.reports.push(text);
-    }
-    printReports() {
-        console.log(this.reports);
-    }
-}
-const accounting = new AccountingDepartment('d2', []);
-console.log(accounting);
-//# sourceMappingURL=app.js.map
+var user1;
+user1 = {
+    name: 'Barbie',
+    age: 20,
+    greeting: function (text) {
+        console.log("".concat(text, " ").concat(this.name));
+    },
+};
