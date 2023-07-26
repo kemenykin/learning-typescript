9c23033c0389406b96736f330638bd428f000aac
kemenykin <kemenykin@gmail.com>
2023-07-26 11:13:01 +0200

    typeguards code

--- 06-advanced-types/src/app.ts
+++ 06-advanced-types/src/app.ts
@@ -1,3 +1,5 @@
+// we have a type Admin - which is an object type
+
 type Admin = {
   name: string;
   privileges: string[];
@@ -5,20 +7,50 @@ type Admin = {
 
 type Employee = {
   name: string;
-  startDate: Date;
-};
-
-// interface ElevatedEmployee extends Employee, Admin {}
-
-type ElevatedEmployee = Admin & Employee;
-
-const e1: ElevatedEmployee = {
-  name: 'Max',
-  privileges: ['create-server'],
-  startDate: new Date()
-};
+  startDate: Date;  // it is a supported type by TS
+}
 
 type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+// combination of these two types from above - has a name, a startDate, and privileges
+
+type ElevatedEmployee = Admin & Employee;
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+// instanceof
+
+class Car {
+  drive() {
+    console.log('Driving');
+  }
+}
+
+class Truck {
+  drive() {
+    console.log('Driving');
+  }
+  loadCargo(amount: number) {
+    console.log('Loading cargo ' + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();  // both of them knows this method
+  // vehicle.loadCargo(); error - only the Truck has this
+}
